dc404713c8728726da7c0ae7d917f720000de92b
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-07-13 19:55:17 -0500

    Add the top header

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- ============================================================================== --
+-- ----------------------     RocketVim Loading plugins     --------------------- --
+-- ============================================================================== --
+
 if vim.fn.has('win32') or vim.fn.has('win64') then
   vim.cmd('source ~/appdata/local/nvim/manager.vim')
 elseif vim.fn.has('unix') or vim.fn.has('linux') then
