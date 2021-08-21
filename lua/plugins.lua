984045986cf2c75edbfbd6c40ce3ef808029da88
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-08-20 20:22:38 -0500

    fix: go support attach

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -227,10 +227,10 @@ use {
       tag_transform = false,
       test_dir = '',
       comment_placeholder = '   ',
-      lsp_cfg = true, -- false: use your own lspconfig
+      lsp_cfg = false, -- false: use your own lspconfig
       lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
-      lsp_on_attach = true, -- use on_attach from go.nvim
-      dap_debug = true,
+      lsp_on_attach = false, -- use on_attach from go.nvim
+      dap_debug = false,
     }
     vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').gofmt() ]], false)
     vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
