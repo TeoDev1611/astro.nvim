084d6fd14028a9be25fb637c664252f055cc76a9
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-08-22 16:49:45 -0500

    style: format the code

--- lua/ide/tree-sitter/init.lua
+++ lua/ide/tree-sitter/init.lua
@@ -2,20 +2,20 @@ require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
   },
-   ensure_installed = {
-      "lua",
-      "markdown"
-   },
+  ensure_installed = {
+    'lua',
+    'markdown',
+  },
   rainbow = {
     enable = true,
   },
 }
 
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.jsonc.used_by = "json"
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+parser_config.jsonc.used_by = 'json'
 parser_config.markdown = {
   install_info = {
-    url = "https://github.com/ikatyang/tree-sitter-markdown",
-    files = { "src/parser.c", "src/scanner.cc" },
+    url = 'https://github.com/ikatyang/tree-sitter-markdown',
+    files = { 'src/parser.c', 'src/scanner.cc' },
   },
 }
