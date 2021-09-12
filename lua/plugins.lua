ec2c196b951809a63e4ce9eb9924d0a74d0a4d01
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-09-12 15:57:53 -0500

    feat: add boostrap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,10 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
 vim.cmd [[packadd packer.nvim]]
 local packer = require 'packer'
 
@@ -128,7 +135,7 @@ use {
 
 use { 'tpope/vim-fugitive', cmd = { 'G' }, opt = true }
 
--- Lang Configs
+---- Lang Configs
 use { 'editorconfig/editorconfig-vim' }
 use { 'kyazdani42/nvim-web-devicons' }
 
