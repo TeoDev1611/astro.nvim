67b5647e7668929fc3f3239f66445cd6e5e320d7
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-09-28 12:51:29 -0500

    fix: deprecation message nvim tree.lua

--- lua/ui/tree/init.lua
+++ lua/ui/tree/init.lua
@@ -7,22 +7,19 @@ tree.setup {
   lsp_diagnostics = true,
   auto_close = true,
   update_cwd = false,
+  ignore_ft_on_setup  = {'alpha'},
+  view = {
+    width = 30,
+    side = 'left',
+    auto_resize = true
+  }
 }
 
-g.nvim_tree_side = 'left'
-g.nvim_tree_width = 30
 g.nvim_tree_ignore = { '.git', 'node_modules', '.cache', 'pack', 'extension' }
-g.nvim_tree_auto_open = 1
-g.nvim_tree_auto_close = 1
-g.nvim_tree_quit_on_open = 0
-g.nvim_tree_follow = 1
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_hide_dotfiles = 0
 g.nvim_tree_git_hl = 1
 g.nvim_tree_root_folder_modifier = ':t'
-g.nvim_tree_tab_open = 0
-g.nvim_tree_allow_resize = 1
-g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' }
 
 g.nvim_tree_show_icons = {
   git = 1,
@@ -31,7 +28,7 @@ g.nvim_tree_show_icons = {
   folder_arrows = 0,
 }
 
-vim.g.nvim_tree_icons = {
+g.nvim_tree_icons = {
   default = '',
   symlink = '',
   git = {
