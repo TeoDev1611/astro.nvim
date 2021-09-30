8028d0e371ee2d35d889a1c9e37f5ba84c766356
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-09-30 17:00:41 -0500

    feat: clean the plugins unused

--- lua/ui/lualine/init.lua
+++
@@ -1,9 +0,0 @@
-require('lualine').setup {
-  options = {
-    theme = 'auto',
-    component_separators = { '', '' },
-    section_separators = { '', '' },
-    icons_enabled = true,
-  },
-  extensions = { 'nvim-tree' },
-}
